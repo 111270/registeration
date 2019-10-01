83b33dcdc0d364c5213acbfe47937308797326f3
111270 <42199146+111270@users.noreply.github.com>
2019-10-01 23:11:38 +0300

    Create main.php

---
+++ main.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<style>
+		
+body {
+	background-image: url("main4.jpeg");
+	height: 100%
+
+	background-position: center;
+	 background-repeat: no-repeat;
+  background-size: cover;
+	 }
+
+
+.wrapper {
+    text-align: center;
+  
+}
+
+.button {
+  border-radius: 4px;
+  background-color: #f4511e;
+  border: none;
+  color: #FFFFFF;
+  text-align: center;
+  font-size: 28px;
+  padding: 20px;
+  width: 200px;
+  transition: all 0.5s;
+  cursor: pointer;
+  margin: 5px;
+}
+
+h1 {
+  color: white;
+  font-family: ;
+  font-size: 500%;
+  text-align: center;
+  padding: 30px;
+}
+	</style>
+</head>
+<body>
+	<h1>Hey Studee</h1>
+<div class="wrapper">
+<button class="button">Log in<a href="signup.php"></button>
+<br><br><br><br>
+<button class="button">Register</button>
+</div>
+</body>
+</html>
