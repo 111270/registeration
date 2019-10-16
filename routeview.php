1d73aaa013b3c5b4cd0e86aa198f1bea62c11055
111270 <42199146+111270@users.noreply.github.com>
2019-10-16 12:40:32 +0300

    Create routeview.php

---
+++ routeview.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+  <style>
+    
+    body {
+  background-image: url("dashb.jpg");
+  height: 100%
+
+  background-position: center;
+   background-repeat: no-repeat;
+  background-size: cover;
+   }
+
+  </style>
+  <title>Routeview</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+</head>
+<body>
+  <div class="container">
+    <div class="Login-box">
+    <div class="row">
+      <div class="col-md-6">
+<h1>Please Input your route</h1>
+      <center>  <label>Start Location</label>
+<input type="text" name="Address" class="form-control" required>
+<br>
+<label>End location</label>
+<input type="text" name="Address" class="form-control" required>
+<br>
+
+
+<button type="submit">View</button>
+
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
