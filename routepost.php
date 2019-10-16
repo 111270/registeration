a64599fe4111eee5269515abe7d97426d7d5e20c
111270 <42199146+111270@users.noreply.github.com>
2019-10-16 12:41:04 +0300

    Create routepost.php

---
+++ routepost.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+  <style>
+    
+    body {
+  background-image: url("nairobi.jpg");
+  height: 100%
+
+  background-position: center;
+   background-repeat: no-repeat;
+  background-size: cover;
+   }
+
+  </style>
+  <title>Routepost</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+</head>
+<body>
+  <div class="container">
+    <div class="Login-box">
+    <div class="row">
+      <div class="col-md-6">
+<h1>Please Input your route details</h1>
+      <center>  <label>Start Location</label>
+<input type="text" name="Address" class="form-control" required>
+<br>
+<label>End location</label>
+<input type="text" name="Address" class="form-control" required>
+<br>
+<label>Time details</label>
+<input type="text" name="Address" class="form-control" required></center>
+
+<button type="submit">Post</button>
+
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
